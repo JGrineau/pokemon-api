1af4160a2ceeaabdbd653f1f574bddbc17be4c39
JP Grineau <jgrineau95@gmail.com>
2024-11-19 22:01:55 -0500

    signup page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/nav-bar/NavBar';
 import Login from './components/login-page/Login';
 import PokemonList from './components/pokemon-list/Pokemonlist';
 import PokemonDetails from './components/pokemon-details/PokemonDetails';
+import Signup from './components/signup-page/Signup';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<PokemonList />} />
         <Route path="/login" element={<Login />} />
         <Route path="/pokemon/:id" element={<PokemonDetails />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
